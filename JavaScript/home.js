16ffbaae70882fcd0fb450e26d5b016f24fef163
mohammadradad433 <s12027855@stu.najah.edu>
2025-02-26 11:21:56 +0200

    up

--- JavaScript/home.js
+++ JavaScript/home.js
@@ -1,3 +1,14 @@
+const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.onmouseenter = Swal.stopTimer;
+      toast.onmouseleave = Swal.resumeTimer;
+    }
+  });
 function openAltman() {
     window.location.href="Altman.html";
 }
@@ -12,6 +23,10 @@ function openTaffler() {
 
 function exitApp() {
     if (confirm("Are you sure you want to exit?")) {
+        Toast.fire({
+            icon: "success",
+            title: "Signed in successfully"
+          });
         window.close();
     }
 }
